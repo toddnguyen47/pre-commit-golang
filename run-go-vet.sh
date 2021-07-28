1935564a139834381db7a7b461a6d765a70903c1
Todd Nguyen <todd.nguyen47@gmail.com>
2021-07-28 14:01:58 -0700

    Change `run-go-vet` to run on package instead of individual file

--- run-go-vet.sh
+++ run-go-vet.sh
@@ -6,7 +6,8 @@ _root_dir="$(git rev-parse --show-toplevel)"
 pushd "${_root_dir}" > /dev/null
 
 exec 5>&1
-output="$(go vet "$@" | tee /dev/fd/5)"
+_package_name="$(echo $@|xargs -n1 dirname)"
+output="$(go vet "${_package_name}/*.go" | tee /dev/fd/5)"
 [[ -z "$output" ]]
 
 popd > /dev/null

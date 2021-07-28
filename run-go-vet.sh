1c598db40137697d91634e17f4aa6a509e5ba4b1
Todd Nguyen <todd.nguyen47@gmail.com>
2021-07-28 16:39:21 -0700

    For `run-go-get` and `run-golangci-lint`, split out the
    command line arguments into a for loop

--- run-go-vet.sh
+++ run-go-vet.sh
@@ -6,8 +6,11 @@ _root_dir="$(git rev-parse --show-toplevel)"
 pushd "${_root_dir}" > /dev/null
 
 exec 5>&1
-_package_name="$(echo $@ | xargs -n1 dirname)"
-output="$(go vet ${_package_name}/*.go | tee /dev/fd/5)"
-[[ -z "$output" ]]
+
+for vararg in "$@"; do
+  _package_name="$(echo ${vararg} | xargs -n1 dirname)"
+  output="$(go vet ${_package_name}/*.go | tee /dev/fd/5)"
+  [[ -z "$output" ]]
+done
 
 popd > /dev/null
--- run-golangci-lint.sh
+++ run-golangci-lint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 set -e -o pipefail
 
-_package_name="$(echo $@ | xargs -n1 dirname)"
-exec golangci-lint run "${_package_name}"
+for vararg in "$@"; do
+  _package_name="$(echo ${vararg} | xargs -n1 dirname)"
+  exec golangci-lint run "${_package_name}"
+done
